0de8be2f0228b577786104d8c738260d956f48ab
Alexandre Silva <contato@silvalexandre.com>
2017-01-29 16:14:37 -0200

    es6 code fixes

--- index.js
+++ index.js
@@ -1,51 +1,40 @@
-const grib = require(__dirname + '/grib'),
-	express = require('express'),
-	app = express(),
-	multer = require('multer'),
-	jade = require('jade'),
-	fs = require('fs'),
-	upload = multer({
+(function() {
+	'use strict';
+	const grib = require(__dirname + '/grib');
+	const express = require('express');
+	const app = express();
+	const multer = require('multer');
+	const jade = require('jade');
+	const fs = require('fs');
+	const upload = multer({
 		dest: './grib_files/'
 	});
-
-app.set('view engine', 'jade');
-
-app.get('/', (req, res) => {
-	res.render(__dirname + '/pages/index');
-});
-
-app.post('/upload', upload.single('grib_file'), (req, res) => {
-	const grib_hash = Date.now(),
-		fileName = "grib_" + grib_hash + ".grb";
-
-	fs.renameSync(__dirname + "/grib_files/" + req.file.filename, __dirname + "/grib_files/" + fileName);
-
-	res.redirect('/check?grib_hash=' + grib_hash);
-});
-
-app.get('/check', (req, res) => {
-	const grib_hash = req.query.grib_hash;
-
-	grib.load('./grib_files/grib_' + grib_hash + '.grb', (err, data) => {
-		if(err) throw err;
-
-		var response = [];
-
-		for(var i = 0; i < data.length; ++i) {
-			response.push(data[i].message);
-		}
-		res.render(__dirname + '/pages/result', {
-			action: 'Checking file',
-			hashKey: grib_hash,
-			messages: response
+	app.set('view engine', 'jade');
+	app.get('/', (req, res) => {
+		res.render(__dirname + '/pages/index');
+	});
+	app.post('/upload', upload.single('grib_file'), (req, res) => {
+		const grib_hash = Date.now();
+		const fileName = "grib_" + grib_hash + ".grb";
+		fs.renameSync(__dirname + "/grib_files/" + req.file.filename, __dirname + "/grib_files/" + fileName);
+		res.redirect('/check?grib_hash=' + grib_hash);
+	});
+	app.get('/check', (req, res) => {
+		const grib_hash = req.query.grib_hash;
+		grib.load('./grib_files/grib_' + grib_hash + '.grb', (err, data) => {
+			if (err) throw err;
+			var response = [];
+			for (var i = 0; i < data.length; ++i) {
+				response.push(data[i].message);
+			}
+			res.render(__dirname + '/pages/result', {
+				action: 'Checking file',
+				hashKey: grib_hash,
+				messages: response
+			});
 		});
 	});
-});
-
-var server = app.listen(8080, () => {
-	const host = server.address()
-		.address,
-		port = server.address()
-		.port;
-	console.log('Server running at http://localhost/' + port);
-});
+	const server = app.listen(8080, () => {
+		console.log('Server running at http://localhost:8080/');
+	});
+})();
